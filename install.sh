deb52b9e2332d19e585caca4d26f9325b121f2ad
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-10-11 13:33:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,8 +23,16 @@ IPK="luci-app-radius-monitor_${TAG}_all.ipk"
 curl -s -L -o "/tmp/${IPK}" "https://mirror.ghproxy.com/https://github.com/rtaserver/RadiusMonitor/releases/latest/download/${IPK}"
 
 # install ipks
-echo "install ipks"
-opkg install /tmp/${IPK}
+# check if the package is already installed and its version
+INSTALLED_VERSION=$(opkg info luci-app-radius-monitor | grep 'Version:' | cut -d ' ' -f 2)
+
+if [ "$INSTALLED_VERSION" = "$TAG" ]; then
+    echo "The same version ($TAG) is already installed, forcing reinstallation"
+    opkg install --force-reinstall /tmp/${IPK}
+else
+    echo "installing version $TAG"
+    opkg install /tmp/${IPK}
+fi
 if [ $? -ne 0 ]; then
     clear
     echo "Installation failed"
